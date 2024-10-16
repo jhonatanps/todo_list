a873a64223d9b990e381f798a37e30e9ff14fb33
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-10-16 08:52:30 -0300

    alert

---
+++ script.js
@@ -0,0 +1,12 @@
+const novaTarefa = document.getElementById('novaTarefa');
+const btnAddTarefa = document.getElementById('btnAddTarefa');
+const listaTarefa = document.getElementById('listaTarefa');
+
+btnAddTarefa.addEventListener('click', function(){
+
+    alert('Bem vindo');
+
+
+
+
+});
