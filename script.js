58675906613568b6759bcc901a77af5668db64ba
Jhonatan Padua Soares <61254467+jhonatanps@users.noreply.github.com>
2024-10-16 10:10:24 -0300

    logica

--- script.js
+++ script.js
@@ -3,8 +3,31 @@ const btnAddTarefa = document.getElementById('btnAddTarefa');
 const listaTarefa = document.getElementById('listaTarefa');
 
 btnAddTarefa.addEventListener('click', function(){
+    const tarefa = novaTarefa.value;
+    
+    // se a tarefa for diferente de vazia ele execulta
+    if(tarefa != " "){
 
-    alert(novaTarefa);
+        const li = document.createElement('li');
+
+        li.textContent = novaTarefa;
+
+        const botaoRemover = document.createElement('button');
+        botaoRemover.textContent = "Remover";
+
+        botaoRemover.addEventListener('click', function(){
+            li.remove();
+        });
+
+        
+        li.appendChild(botaoRemover);
+        listaTarefa.appendChild(li);
+
+        novaTarefa.value = "";
+
+
+    }
+
 
 
 
